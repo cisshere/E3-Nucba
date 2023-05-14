da026001616381585d512ba2e175422754f8eaa9
cisshere <cis.shere@gmail.com>
2023-05-14 16:45:48 -0300

    agregando ingredientes a la card

--- index.js
+++ index.js
@@ -47,51 +47,105 @@ const pizzas = [
 ];
 
 
- const agregarClick = () => {
+
+
+const error = (msj) => {
+  const contenedor = document.querySelector(".contenedor");
+    contenedor.innerHTML = "";
+
+  const inputNumber = document.querySelector("#input-number");
+  inputNumber.classList.add("msj-error");
+
+  const contenedorError = document.querySelector(".contenedor-error");
+  contenedorError.innerHTML = "";
+  
+  const small = document.createElement("small");
+  small.classList.add("msj-error");
+  small.innerText = msj;
+
+  contenedorError.appendChild(small);
+}
+    
+const LS_DATOS = "LS_DATOS";
+
+
+ const mostrarPizza = (pizza) => {
+
+  const contenedor = document.querySelector(".contenedor");
+    contenedor.innerHTML = "";
+
+  const div = document.createElement("div");
+           div.classList.add("contenedor-pizza");
+        
+            const h3 = document.createElement("h3");
+            h3.classList.add("nombre-pizza");
+            h3.innerText = pizza.nombre;
+    
+            const img = document.createElement("img");
+            img.classList.add("imagen-pizza");
+            img.src = pizza.imagen;
+    
+            const p = document.createElement("p");
+            p.classList.add("precio-pizza");
+            p.textContent = pizza.precio;
+
+            contenedor.appendChild(div);
+            div.appendChild(h3);
+            div.appendChild(img);
+            div.appendChild(p);
+
+ }
+
+ const buscarPizza = () => {
     const inputNumber = document.querySelector("#input-number");
     let number = inputNumber.value;
-    let sonIguales = false;
+    inputNumber.classList.remove("msj-error");
+
+    const contenedorError = document.querySelector(".contenedor-error");
+    contenedorError.innerHTML = "";
+
+    if (number.trim() === ""){
+      error("Ingresa un numero");
+      return;
+    }
+
     
     for (let i = 0; i < pizzas.length; i++) {
-      if (pizzas[i].id == number) {
-        console.log("hola");
+      const pizza = pizzas[i];
+      if (pizza.id == number) {
 
-      const contenedor = document.querySelector(".contenedor");
-        
-                const h3 = document.createElement("h3");
-                h3.classList.add("nombre-pizza");
-                h3.innerText = pizzas[i].nombre;
-        
-                const img = document.createElement("img");
-                img.classList.add("imagen-pizza");
-                img.src = pizzas[i].imagen;
-        
-                const p = document.createElement("p");
-                p.classList.add("precio-pizza");
-                p.textContent = pizzas[i].precio;
-        
-                contenedor.appendChild(h3);
-                contenedor.appendChild(img);
-                contenedor.appendChild(p);
+        mostrarPizza(pizza);
+        ingredientesPizza(pizza);
 
-                return;
-      }
-      else if (inputNumber.value.trim() === ""){
-        console.log("el contenedor esta vacio");
-        return;
+            localStorage.setItem(LS_DATOS,JSON.stringify(pizza));
+            return;
       }
     }
     
-    console.log("bye");
+    error("No se encontro pizza con ese numero");
 
   
 } 
 
+init = () => {
 
-/*
-<h3>Pizza</h3>
-            <img src="img/4quesos.png" alt="">
-            <p>$1500</p> 
+  const pizzaString = localStorage.getItem(LS_DATOS);
+  if(pizzaString !== null){
+    const pizza = JSON.parse(pizzaString);
+    mostrarPizza(pizza);
+  }
+  
+}
 
-*/
+init();
 
+ingredientesPizza = (pizza) => {
+
+  const contenedor = document.querySelector(".contenedor-pizza");
+
+  const p = document.createElement("p");
+            p.classList.add("ingredientes");
+            p.innerText = pizza.ingredientes;
+
+ contenedor.appendChild(p);
+}
